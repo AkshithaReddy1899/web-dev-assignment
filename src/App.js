d3da389e1441accc2a409127563e4351c3aef894
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-10-28 11:04:29 +0530

    Revert "Display images"
    This reverts commit e46847a01d34062c8400ac914584cb86d702e5df.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Main from './components/Main';
 
 function App() {
   return (
-    <div className="bg-slate-900 text-white p-10 w-screen h-screen  overflow-hidden">
+    <div className="bg-red-900">
       <Main />
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,30 +4,15 @@ import { fetchImages } from '../feature/DataSlice';
 
 export const Main = () => {
 	const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(fetchImages());
+    dispatch(fetchImages())
   },[dispatch])
 
 	const data = useSelector((state) => state.dataReducer)
 	console.log(data);
-
+	
 	return (
-		<div className="text-center">
-			<div className="main-container">
-				{
-				data.displayImages.length>0	? (<div className="card-container">
-						{
-							data.displayImages.map((item) => (
-								<div key={item.id} className="image m-2" style={{background:`url("${item.url}")no-repeat center/cover`}}>
-								</div>
-							))
-						}
-					</div>
-					): (<p>Something went wrong try agian</p>)
-				}
-		  </div>
-		</div>
+		<div>main</div>
 	)
 }
 
